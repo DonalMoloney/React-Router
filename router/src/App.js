ac543354a2e7e2b897894ec2e8d40739b7f51c9c
DonalMoloney <Donalmshs@gmail.com>
2019-04-10 13:52:21 -0500

    Created a home page for when the url is only localhost:3000/ and have it implemnted and displaying home

--- router/src/App.js
+++ router/src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/Home"
 
-const NewRoute = () =>{
-  return(
-    <div>
-      <p> New Route </p>
-    </div>
-  );
-}
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path="/new" component={NewRoute} /> //Essentially routing in a nutshell you create routes and components and link them togetehr
+        <Route path="/" component={Home}/>
       </BrowserRouter>
     );
   }
---
+++ router/src/components/Home.js
@@ -0,0 +1,13 @@
+//Component that shows up on the home page when there is no url
+
+import React from "react";
+
+const Home = () =>{
+  return (
+    <div>
+      <p>Home</p>
+    </div>
+  );
+};
+
+export default Home
